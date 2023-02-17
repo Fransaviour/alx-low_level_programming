be2857962af92f2bbf8bae1cc090687f399eb9db
Fransaviour <fransaviourchebet@gmail.com>
2023-02-17 13:48:24 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Successs)
+ */
+
+int main(void)
+
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}
